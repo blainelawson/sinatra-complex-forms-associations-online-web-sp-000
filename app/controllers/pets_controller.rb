93a8eeaa24d12494472e9185a52530a19738bd60
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-22 16:15:23 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -34,7 +34,6 @@ class PetsController < ApplicationController
   patch '/pets/:id' do
     @pet = Pet.find_by_id(params[:id])
     @pet.name = params[:pet][:name]
-    binding.pry
     if !params["owner_name"].empty?
       @pet.owner = Owner.create(name: params["owner_name"])
     else
