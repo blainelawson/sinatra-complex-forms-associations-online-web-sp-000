eff0c68e0f94565f075f852f14abdcfb632bdbe3
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-22 15:34:27 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -14,7 +14,6 @@ class PetsController < ApplicationController
   post '/pets' do
     @pet = Pet.create(params[:pet])
     if !params["owner_name"].empty?
-      # binding.pry
       @pet.owner = Owner.create(name: params["owner_name"])
       @pet.save
     end
